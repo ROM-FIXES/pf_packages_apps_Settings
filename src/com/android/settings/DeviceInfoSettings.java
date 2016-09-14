b12e297dac415afe47082b1ac270444f4e0adadb
mnm9994u <mnm9994u@gmail.com>
2016-09-15 02:37:12 +0530

    Add Cpu & Mem Info
    Change-Id: I12763a8df5db4dd52edbb9a169b568de2145d2dc
    Signed-off-by: Varun Date <date.varun123@gmail.com>
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -44,6 +44,11 @@ import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -79,6 +84,10 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
     private static final String KEY_MOD_BUILD_COMPILER_SDCLANG = "build_compiler_sdclang";
     private static final String KEY_FUSION_LOGO = "ic_settings_fusion";
     private static final String KEY_FUSION_CHANGELOG = "fusion_changelog";
+    private static final String FILENAME_PROC_MEMINFO = "/proc/meminfo";
+    private static final String FILENAME_PROC_CPUINFO = "/proc/cpuinfo";
+    private static final String KEY_DEVICE_CPU = "device_cpu";
+    private static final String KEY_DEVICE_MEMORY = "device_memory";
 
     /**
      * For Search.
@@ -201,6 +210,25 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
         // Remove selinux information if property is not present
         removePreferenceIfPropertyMissing(getPreferenceScreen(), KEY_SELINUX_STATUS,
                 PROPERTY_SELINUX_STATUS);
+
+    String cpuInfo = getCPUInfo();
+        String memInfo = getMemInfo();
+
+        if (cpuInfo != null) {
+            setStringSummary(KEY_DEVICE_CPU, cpuInfo);
+        } else {
+            getPreferenceScreen().removePreference(findPreference(KEY_DEVICE_CPU));
+        }
+
+        if (memInfo != null) {
+            setStringSummary(KEY_DEVICE_MEMORY, memInfo);
+        } else {
+            getPreferenceScreen().removePreference(findPreference(KEY_DEVICE_MEMORY));
+        }
+
+        // Remove Safety information preference if PROPERTY_URL_SAFETYLEGAL is not set
+        removePreferenceIfPropertyMissing(getPreferenceScreen(), KEY_SAFETY_LEGAL,
+                PROPERTY_URL_SAFETYLEGAL);
 
         // Remove Equipment id preference if FCC ID is not set by RIL
         removePreferenceIfPropertyMissing(getPreferenceScreen(), KEY_EQUIPMENT_ID,
@@ -407,6 +435,61 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
                 mSummaryLoader.setSummary(this, mContext.getString(R.string.about_summary,
                         Build.VERSION.RELEASE));
             }
+        }
+    }
+
+    private String getMemInfo() {
+            String result = null;
+            BufferedReader reader = null;
+    
+            try {
+                /* /proc/meminfo entries follow this format:
+                 * MemTotal:         362096 kB
+                 * MemFree:           29144 kB
+                 * Buffers:            5236 kB
+                 * Cached:            81652 kB
+                 */
+                String firstLine = readLine(FILENAME_PROC_MEMINFO);
+                if (firstLine != null) {
+                    String parts[] = firstLine.split("\\s+");
+                    if (parts.length == 3) {
+                        result = Long.parseLong(parts[1])/1024 + " MB";
+                    }
+                }
+            } catch (IOException e) {}
+    
+            return result;
+        }
+    
+        private String getCPUInfo() {
+            String result = null;
+    
+            try {
+                /* The expected /proc/cpuinfo output is as follows:
+                 * Processor    : ARMv7 Processor rev 2 (v7l)
+                 * BogoMIPS    : 272.62
+                 */
+                String firstLine = readLine(FILENAME_PROC_CPUINFO);
+                if (firstLine != null) {
+                    result = firstLine.split(":")[1].trim();
+                }
+            } catch (IOException e) {}
+    
+            return result;
+        }
+
+    /**
+     * Reads a line from the specified file.
+     * @param filename the file to read from
+     * @return the first line, if any.
+     * @throws IOException if the file couldn't be read
+     */
+    private static String readLine(String filename) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(filename), 256);
+        try {
+            return reader.readLine();
+        } finally {
+            reader.close();
         }
     }
 

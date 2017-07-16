5f94ac786cf75c4acc17e775fe240d8ce0aad44b
Eliminater74 <eliminater74@gmail.com>
2017-07-16 00:34:37 -0400

    [device info] Display GCC/Clang/SDClang Versions: [1/2]
    Re-work for O
    
    Change-Id: I33faf5290c9da5f726b0bf7e307ff0b8c594431b
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>
    Signed-off-by: Tab Fitts <tfitts1212@gmail.com>

---
+++ src/com/android/settings/deviceinfo/GCCVersionPreferenceController.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2017 Pure Fusion OS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.settings.deviceinfo;
+
+import android.content.Context;
+import android.os.SystemProperties;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
+import android.text.TextUtils;
+
+import com.android.settings.R;
+import com.android.settings.core.PreferenceController;
+
+public class GCCVersionPerferenceController extends PreferenceController {
+
+    private static final String PROPERTY_MOD_BUILD_COMPILER_GCC = "ro.build.fusion.gcc";
+    private static final String KEY_MOD_BUILD_COMPILER_GCC = "build_compiler_gcc";
+
+    public GCCVersionPerferenceController(Context context) {
+        super(context);
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return !TextUtils.isEmpty(SystemProperties.get(PROPERTY_MOD_BUILD_COMPILER_GCC));
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return KEY_MOD_BUILD_COMPILER_GCC;
+    }
+
+    @Override
+    public void displayPreference(PreferenceScreen screen) {
+        super.displayPreference(screen);
+        final Preference pref = screen.findPreference(KEY_MOD_BUILD_COMPILER_GCC);
+        if (pref != null) {
+            final String summary = SystemProperties.get(PROPERTY_MOD_BUILD_COMPILER_GCC,
+                    mContext.getResources().getString(R.string.build_compiler_gcc_default));
+            pref.setSummary(summary);
+        }
+    }
+}

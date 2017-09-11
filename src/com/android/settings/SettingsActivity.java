vansaiyuki/abc-n_packages_apps_Settings

Allow to quickly open Running Services from notification/qs panel [2/2]

src/com/android/settings/SettingsActivity.java
@@ -69,6 +69,7 @@ import com.android.settings.applications.ManageDomainUrls;
 import com.android.settings.applications.NotificationApps;
 import com.android.settings.applications.ProcessStatsSummary;
 import com.android.settings.applications.ProcessStatsUi;
+import com.android.settings.applications.RunningServices;
 import com.android.settings.applications.UsageAccessDetails;
 import com.android.settings.applications.VrListenerSettings;
 import com.android.settings.applications.WriteSettingsDetails;
@@ -267,6 +268,7 @@ public class SettingsActivity extends SettingsDrawerActivity
             Settings.PaymentSettingsActivity.class.getName(),
             //custom_section
             Settings.AbcSettingsActivity.class.getName(),
+            Settings.DevRunningServicesActivity.class.getName(),
     };
 
     private static final String[] ENTRY_FRAGMENTS = {
@@ -371,7 +373,8 @@ public class SettingsActivity extends SettingsDrawerActivity
             NavbarSettings.class.getName(),
             FlingSettings.class.getName(),
             SmartbarSettings.class.getName(),
-            PulseSettings.class.getName()
+            PulseSettings.class.getName(),
+            RunningServices.class.getName(),
     };
 
 

ROM-FIXES/pf_packages_apps_Settings

Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>
Conflicts:
	src/com/android/settings/Settings.java
	src/com/android/settings/SettingsActivity.java
	src/com/android/settings/dashboard/DashboardAdapter.java
Change-Id: Iee6500196b2762dc538046b575fb3b22f7c43b57

src/com/android/settings/SettingsActivity.java
@@ -70,6 +70,7 @@ import com.android.settings.applications.ManageDomainUrls;
 import com.android.settings.applications.NotificationApps;
 import com.android.settings.applications.ProcessStatsSummary;
 import com.android.settings.applications.ProcessStatsUi;
+import com.android.settings.applications.RunningServices;
 import com.android.settings.applications.UsageAccessDetails;
 import com.android.settings.applications.VrListenerSettings;
 import com.android.settings.applications.WriteSettingsDetails;
@@ -325,7 +326,8 @@ public class SettingsActivity extends SettingsDrawerActivity
             DisplayRotation.class.getName(),
             ExpandedDesktopPreferenceFragment.class.getName(),
             HeadsUpSettings.class.getName(),
-            AmbientSettings.class.getName()
+            AmbientSettings.class.getName(),
+            RunningServices.class.getName()
     };
     private static final String[] LIKE_SHORTCUT_INTENT_ACTION_ARRAY = {
             "android.settings.APPLICATION_DETAILS_SETTINGS"
@@ -376,6 +378,7 @@ public class SettingsActivity extends SettingsDrawerActivity
             Settings.AccessibilitySettingsActivity.class.getName(),
             Settings.PrintSettingsActivity.class.getName(),
             Settings.PaymentSettingsActivity.class.getName(),
+            Settings.DevRunningServicesActivity.class.getName(),
     };
     private SharedPreferences mDevelopmentPreferences;
     private SharedPreferences.OnSharedPreferenceChangeListener mDevelopmentPreferencesListener;
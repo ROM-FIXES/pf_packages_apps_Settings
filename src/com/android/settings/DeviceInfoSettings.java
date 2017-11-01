c6c6310a0a69849aa100262f05b84a316b94f18c
Tab Fitts <tfitts1212@gmail.com>
2017-11-01 13:35:25 -0700

    [settings] Add PureFusion OTA
    Change-Id: I238de7ff056d36b305668b524fd373faafe25d74

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -27,6 +27,7 @@ import com.android.settings.core.PreferenceController;
 import com.android.settings.core.lifecycle.Lifecycle;
 import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.dashboard.SummaryLoader;
+import com.android.settings.deviceinfo.PureFusionOTAPreferenceController;
 import com.android.settings.deviceinfo.AdditionalSystemUpdatePreferenceController;
 import com.android.settings.deviceinfo.BasebandVersionPreferenceController;
 import com.android.settings.deviceinfo.BuildNumberPreferenceController;
@@ -124,6 +125,7 @@ public class DeviceInfoSettings extends DashboardFragment implements Indexable {
         controllers.add(
                 new BuildNumberPreferenceController(context, activity, fragment, lifecycle));
         controllers.add(new AdditionalSystemUpdatePreferenceController(context));
+        controllers.add(new PureFusionOTAPreferenceController(context));
         controllers.add(new ManualPreferenceController(context));
         controllers.add(new KernelVersionPreferenceController(context));
         controllers.add(new BasebandVersionPreferenceController(context));

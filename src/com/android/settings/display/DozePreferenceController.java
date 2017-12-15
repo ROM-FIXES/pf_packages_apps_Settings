a4e391e4ab1e8b8ee9229600c6c09fcf458e741d
INT3NSE07 <d.jonathanpaul@gmail.com>
2017-12-16 04:17:19 +0530

    Revert "Add Ambient display category / Preference controllers to handle switches"
    This reverts commit bf8d0f37e670a7038441d5423bbfc54500928ca9.
    
    Change-Id: I7b3750b1b7ded9e451a157844b7257d963089f48

--- src/com/android/settings/display/DozePreferenceController.java
+++ src/com/android/settings/display/DozePreferenceController.java
@@ -70,11 +70,15 @@ public class DozePreferenceController extends PreferenceController implements
 
     @Override
     public boolean isAvailable() {
-        String name = Build.IS_DEBUGGABLE ? SystemProperties.get("debug.doze.component") : null;
-        if (TextUtils.isEmpty(name)) {
-            name = mContext.getResources().getString(
-                    com.android.internal.R.string.config_dozeComponent);
+        if (!Utils.hasAltAmbientDisplay(mContext.getApplicationContext())) {
+            String name = Build.IS_DEBUGGABLE ? SystemProperties.get("debug.doze.component") : null;
+            if (TextUtils.isEmpty(name)) {
+                name = mContext.getResources().getString(
+                        com.android.internal.R.string.config_dozeComponent);
+            }
+            return !TextUtils.isEmpty(name);
+        } else {
+            return false;
         }
-        return !TextUtils.isEmpty(name);
     }
 }
